aebb5c75e965b566ea338c6043765e9ae24a68ae
fkynard2@gmail.com <fkynard2@gmail.com>
2023-08-26 00:07:25 -0400

    Added Binace

--- src/Nav/Projects/BinaceFolder/Binace.js
+++ src/Nav/Projects/BinaceFolder/Binace.js
@@ -1,18 +1,36 @@
 import ProjectNav from "../ProjectNav";
 import Box from "@mui/material/Box";
+import { useState } from "react";
+import LeftContainer from "./LeftContainer";
+import RightImage from "./RightImage";
+import "./binace.css";
+
 
 export default function Binace() {
+    const [stage, setStage] = useState(0);
+    
     return (
         <>
             <Box
                 sx={{
                     width: "100%",
                     height: "100vh",
-                    backgroundColor: "darkgrey",
+                    backgroundColor: "#745e5e",
                 }}
             >
                 <ProjectNav />
-                <div>Binace</div>
+                <Box
+                    sx={{
+                        textAlign: "center",
+                        height: "93vh",
+                        width: "100%",
+                        display: "flex",
+                        justifyContent: "space-between",
+                    }}
+                >
+                    <LeftContainer stage={stage} setStage={setStage} />
+                    <RightImage stage={stage} />
+                </Box>
             </Box>
         </>
     );
